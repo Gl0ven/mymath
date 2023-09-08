14d21f2c09dbccd1297a0cb4808491bf9a6a9d89
Dmitry Demidov <dmitry654demid@mail.ru>
2023-09-08 20:55:41 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Gl0ven/mymath
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,24 @@
+package mymath
+
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
